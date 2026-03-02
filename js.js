bf6f793c6d5143fe0752b8202d384886d9654d72
Kevin Lubbers <kelubbers@gmail.com>
2026-03-01 19:41:22 -0500

    (ui) set up foundation for error messages and result output - main function loop created and working

--- js.js
+++ js.js
@@ -1,9 +1,32 @@
 document.getElementById("form").addEventListener("submit", function(e) {
-    e.preventDefault(); // prevents page reload
+    e.preventDefault();
     decode();
   });
 
   function decode() {
-    let result = document.getElementById("resultSection");
-    result.style.display = "block";
+    if(!checkErrors()){
+        return;
+    }
+    let vin = document.getElementById("vin").value.trim();
+    let resultVIN = document.getElementById("resultVIN");
+    resultVIN.innerHTML = "VIN: " + vin.toUpperCase();
+
+
+    let resultDisplay = document.getElementById("resultSection");
+    resultDisplay.style.display = "block";
+  }
+
+  function checkErrors() {
+    let vin = document.getElementById("vin").value.trim();
+    let errorSection = document.getElementById("errorSection");
+    let error = document.getElementById("errorText");
+    if (vin.length != 17) {
+      errorSection.style.display = "block";
+      error.insertAdjacentHTML("beforeend" , "<p>VIN must be 17 characters long</p>");
+      return false;
+    } else {
+      errorSection.style.display = "none";
+      error.innerHTML = "";
+      return true;
+    }
   }
